9fb7631b3ca3a8638ed08077390eda08c48b9d58
gunalanlakshmanan <gunameha@gmail.com>
2023-09-13 15:03:13 +0530

    Added a new ObjectCountRepo adapter for relational database.

--- counter/adapters/count_repo.py
+++ counter/adapters/count_repo.py
@@ -1,6 +1,8 @@
 from typing import List
 
 from pymongo import MongoClient
+from sqlalchemy import create_engine, Table, select, insert, update
+from sqlalchemy.ext.declarative import declarative_base
 
 from counter.domain.models import ObjectCount
 from counter.domain.ports import ObjectCountRepo
@@ -53,4 +55,40 @@ class CountMongoDBRepo(ObjectCountRepo):
         counter_col = self.__get_counter_col()
         for value in new_values:
             counter_col.update_one({'object_class': value.object_class}, {'$inc': {'count': value.count}}, upsert=True)
+
+
+class CountSQLRepo(ObjectCountRepo):
+
+    def __init__(self, user, pswd, host, port, database):
+        self.db_engine = create_engine(f"postgresql://{user}:{pswd}@{host}:{port}/{database}")
+        base = declarative_base()
+        self.ObjectCounter = Table(
+            'object_counter', base.metadata, autoload_with=self.db_engine, schema='object_detection'
+        )
+
+    def read_values(self, object_classes: List[str] = None) -> List[ObjectCount]:
+        object_counts = []
+        with self.db_engine.connect() as conn:
+            query = select(self.ObjectCounter)
+            result_set = conn.execute(query)
+            if result_set.rowcount:
+                result_set = result_set.all()
+                for row in result_set:
+                    object_counts.append(ObjectCount(row.object_class, row.count))
+        return object_counts
+
+    def update_values(self, new_values: List[ObjectCount]):
+        with self.db_engine.connect() as conn:
+            for value in new_values:
+                query = select(self.ObjectCounter).where(self.ObjectCounter.c.object_class == value.object_class)
+                result = conn.execute(query)
+                if result.rowcount:
+                    query = update(self.ObjectCounter)\
+                        .where(self.ObjectCounter.c.object_class == value.object_class)\
+                        .values(count=self.ObjectCounter.c.count+1)
+                    conn.execute(query)
+                else:
+                    query = insert(self.ObjectCounter).values(object_class=value.object_class)
+                    conn.execute(query)
+            conn.commit()
 
